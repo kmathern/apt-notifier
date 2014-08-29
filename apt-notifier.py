424d589e9e2afaec013088adf19e8e0cd4313160
Kent Mathern <kmathern1@gmail.com>
2014-08-28 20:14:13 -0400

    option to open 'View and Upgrade' with leftclick

--- apt-notifier.py
+++ apt-notifier.py
@@ -15,6 +15,7 @@ message_status = "not displayed"
 # Check for updates, using subprocess.Popen
 def check_updates():
     global message_status
+    global text
     #Create an inline script (what used to be /usr/bin/apt-notifier-check-Updates) and then run it to get the number of updates.
     script = '''#!/bin/sh
     sorted_list_of_upgrades() 
@@ -44,6 +45,10 @@ def check_updates():
     run = subprocess.Popen(["echo -n `sh %s` new updates available" % script_file.name],shell=True, stdout=subprocess.PIPE)
     # Read the output into a text string
     text = run.stdout.read(128)
+    # run = subprocess.Popen(["echo -n 'using upgrade method: '\\\x60apt-get' '$(grep ^UpgradeType ~/.config/apt-notifierrc | cut -f2 -d=)\\\x60"],shell=True, stdout=subprocess.PIPE)
+    # Read the output into a text string
+    # text2 = run.stdout.read(128)
+
     # Alter both Icon and Tooltip, depending on updates available or not 
     if text.startswith( "0" ):
         message_status = "not displayed"  # Resets flag once there are no more updates
@@ -52,25 +57,40 @@ def check_updates():
             AptIcon.hide()
         else:
             AptIcon.setIcon(NoUpdatesIcon)
-            AptIcon.setToolTip(text)
+            AptIcon.setToolTip("0 updates available")
+            #AptIcon.setToolTip("0 updates available\n"+text2)
     else:
-        AptIcon.setIcon(NewUpdatesIcon)
-        AptIcon.show()
-        AptIcon.setToolTip(text)
-        add_upgrade_action()
-        # Shows the pop up message only if not displayed before 
-        if message_status == "not displayed":
-            def show_message():
-                AptIcon.showMessage("Updates", "You have " + text)
-            Timer.singleShot(1000, show_message)
-            message_status = "displayed"
+        if text.startswith( "1 " ):
+            AptIcon.setIcon(NewUpdatesIcon)
+            AptIcon.show()
+            AptIcon.setToolTip("1 new update available")
+            #AptIcon.setToolTip("1 new update available\n"+text2)
+            add_rightclick_upgrade_action()
+            # Shows the pop up message only if not displayed before 
+            if message_status == "not displayed":
+                def show_message():
+                    AptIcon.showMessage("Updates", "You have 1 new update available")
+                Timer.singleShot(1000, show_message)
+                message_status = "displayed"
+        else:
+            AptIcon.setIcon(NewUpdatesIcon)
+            AptIcon.show()
+            AptIcon.setToolTip(text)
+            #AptIcon.setToolTip(text+"\n"+text2)
+            add_rightclick_upgrade_action()
+            # Shows the pop up message only if not displayed before 
+            if message_status == "not displayed":
+                def show_message():
+                    AptIcon.showMessage("Updates", "You have " + text)
+                Timer.singleShot(1000, show_message)
+                message_status = "displayed"
    
-# Define the command to run when clicking Tray Icon
 def start_synaptic():
     run = subprocess.Popen(['/usr/bin/su-to-root -X -c synaptic'],shell=True).wait()
     check_updates()
 
 def showupdates():
+    initialize_aptnotifier_prefs()
     script = '''#!/bin/bash
     DoUpgrade(){
       case $1 in
@@ -136,20 +156,7 @@ def showupdates():
           OtherUpgradeType="upgrade"
         fi
   
-        #test if ~/.config/apt-notifierrc contains a UpgradeAssumeYes line
-        # if it doesn't, add one and initially set it to "false"
-        grep -q ^UpgradeAssumeYes ~/.config/apt-notifierrc
-        if [ "$?" -ne 0 ]; then
-          echo "UpgradeAssumeYes=false">>~/.config/apt-notifierrc
-        fi
         UpgradeAssumeYes=$(grep ^UpgradeAssumeYes ~/.config/apt-notifierrc | cut -f2 -d=)
-
-        #test if ~/.config/apt-notifierrc contains a UpgradeAutoClose line
-        # if it doesn't, add one and initially set it to "false"
-        grep -q ^UpgradeAutoClose ~/.config/apt-notifierrc
-        if [ "$?" -ne 0 ]; then
-          echo "UpgradeAutoClose=false">>~/.config/apt-notifierrc
-        fi
         UpgradeAutoClose=$(grep ^UpgradeAutoClose ~/.config/apt-notifierrc | cut -f2 -d=)
       
         TMP=$(mktemp -d /tmp/apt-notifier.XXXXXX)
@@ -163,7 +170,6 @@ def showupdates():
         sed -i 's/^'$PossiblyWrongNumberOfUpgrades' upgraded, /'$CorrectedNumberOfUpgrades' upgraded, /' "$TMP"/upgrades
         yad \
         --window-icon=/usr/share/icons/mnotify-some.png \
-        --skip-taskbar \
         --width=640 \
         --height=480 \
         --center \
@@ -242,11 +248,11 @@ def showupdates():
 
       done
 
-    sleep 5
+    sleep 2
     PID=`pidof apt-get | cut -f 1 -d " "`
     if [ $PID ]; then
         while (ps -p $PID > /dev/null); do
-            sleep 5
+            sleep 2
         done
     fi
     '''
@@ -256,11 +262,195 @@ def showupdates():
     run = subprocess.Popen(['sh %s' % script_file.name],shell=True).wait()
     check_updates()
 
+def initialize_aptnotifier_prefs():
 
-# Define the action on clicking Tray Icon
-def start_synaptic_activated(reason):
-    if reason == QtGui.QSystemTrayIcon.Trigger:
+    """Create/initialize preferences in the ~/.config/apt-notifierrc file  """
+    """if they don't already exist. Remove multiple entries and those that """
+    """appear to be invalid.                                               """ 
+
+    script = '''#! /bin/bash
+
+    #test if ~/.config/apt-notifierrc contains a UpgradeType=* line and that it's a valid entry
+    grep -q -e ^"UpgradeType=upgrade" -e^"UpgradeType=dist-upgrade" ~/.config/apt-notifierrc
+    if [ "$?" -eq 0 ]
+      then
+      #contains a valid entry so do nothing
+        :
+      else
+      #
+      #if a UpgradeType=* line not present,
+      #or not equal to "upgrade" or "dist-upgrade"
+      #initially set it to "UpgradeType=upgrade"
+      #also delete multiple entries or what appears to be invalid entries
+      sed -i '/.*UpgradeType.*/Id' ~/.config/apt-notifierrc 
+      echo "UpgradeType=upgrade">> ~/.config/apt-notifierrc
+    fi
+
+    #test if ~/.config/apt-notifierrc contains a UpgradeAssumeYes=* line and that it's a valid entry
+    grep -q -e ^"UpgradeAssumeYes=true" -e^"UpgradeAssumeYes=false" ~/.config/apt-notifierrc
+    if [ "$?" -eq 0 ]
+      then
+      #contains a valid entry so do nothing
+        :
+      else
+      #
+      #if a UpgradeAssumeYes=* line not present,
+      #or not equal to "true" or "false"
+      #initially set it to "UpgradeAssumeYes=false"
+      #also delete multiple entries or what appears to be invalid entries
+      sed -i '/.*UpgradeAssumeYes.*/Id' ~/.config/apt-notifierrc 
+      echo "UpgradeAssumeYes=false">> ~/.config/apt-notifierrc
+    fi
+
+    #test if ~/.config/apt-notifierrc contains a UpgradeAutoClose=* line and that it's a valid entry
+    grep -q -e ^"UpgradeAutoClose=true" -e^"UpgradeAutoClose=false" ~/.config/apt-notifierrc
+    if [ "$?" -eq 0 ]
+      then
+      #contains a valid entry so do nothing
+        :
+      else
+      #
+      #if a UpgradeAutoClose=* line not present,
+      #or not equal to "true" or "false"
+      #intially set it to "UpgradeAutoClose=false"
+      #also delete multiple entries or what appears to be invalid entries
+      sed -i '/.*UpgradeAutoClose.*/Id' ~/.config/apt-notifierrc 
+      echo "UpgradeAutoClose=false">> ~/.config/apt-notifierrc
+    fi
+
+    #test if ~/.config/apt-notifierrc contains a LeftClick=* line and that it's a valid entry
+    grep -q -e ^"LeftClick=ViewAndUpgrade" -e^"LeftClick=Synaptic" ~/.config/apt-notifierrc
+    if [ "$?" -eq 0 ]
+      then
+      #contains a valid entry so do nothing
+        :
+      else
+      #
+      #if a LeftClick line not present,
+      #or not equal to "ViewAndUpgrade" or "Synaptic"
+      #initially set it to "LeftClick=Synaptic"
+      #also delete multiple entries or what appears to be invalid entries
+      sed -i '/.*LeftClick.*/Id' ~/.config/apt-notifierrc 
+      echo "LeftClick=Synaptic">> ~/.config/apt-notifierrc
+    fi
+
+    #cleanup any blank lines or lines with only whitespace
+    sed -i '/^[[:space:]]*$/d' ~/.config/apt-notifierrc
+
+    '''
+
+    script_file = tempfile.NamedTemporaryFile('wt')
+    script_file.write(script)
+    script_file.flush()
+    run = subprocess.Popen(['sh %s' % script_file.name],shell=True).wait()
+
+def aptnotifier_prefs():
+    initialize_aptnotifier_prefs()
+    script = '''#! /bin/bash
+    TMP=$(mktemp -d /tmp/apt_notifier_preferences_dialog.XXXXXX)
+    touch "$TMP"/output
+    cat << EOF > "$TMP"/DIALOG
+    <window title="MX Apt Notifier preferences" icon-name="mnotify-some.png">
+      <vbox>
+        <frame  upgrade behaviour   (also affects notification count)   >
+          <radiobutton active="@UpgradeBehaviourAptGetUpgrade@">
+            <label>apt-get upgrade</label>
+            <variable>UpgradeType_upgrade</variable>
+            <action>:</action>
+          </radiobutton>
+          <radiobutton active="@UpgradeBehaviourAptGetDistUpgrade@">
+            <label>apt-get dist-upgrade</label>
+            <variable>UpgradeType_dist-upgrade</variable>
+            <action>:</action>
+          </radiobutton>
+        </frame>
+        <frame  left-click behaviour   (when updates are available)   >
+          <radiobutton active="@LeftClickBehaviourSynaptic@">
+            <label>opens Synaptic </label>
+            <variable>LeftClickSynaptic</variable>
+            <action>:</action>
+          </radiobutton>
+          <radiobutton active="@LeftClickBehaviourViewAndUpgrade@">
+            <label>opens MX Apt Notifier "View and Upgrade" window</label>
+            <variable>LeftClickViewAndUpgrade</variable>
+            <action>:</action>
+          </radiobutton>
+        </frame>
+        <frame>
+          <checkbox active="@UpgradeAssumeYes@">
+            <label>use apt-get --yes option for upgrade/dist-upgrade</label>
+            <variable>UpgradeAssumeYes</variable>
+            <action>:</action>
+          </checkbox>
+        </frame>
+        <frame>
+          <checkbox active="@UpgradeAutoClose@">
+            <label>automatically close terminal window when apt-get upgrade/dist-upgrade complete</label>
+            <variable>UpgradeAutoClose</variable>
+            <action>:</action>
+          </checkbox>
+        </frame>
+      <hbox>
+        <button> <label> Apply </label> <action function="exit">Apply</action> </button>
+        <button cancel> </button>
+      </hbox>
+      </vbox>
+    </window>
+EOF
+
+    # edit placeholders in "$TMP"/DIALOG to set initial settings of the radiobuttons & checkboxes 
+    sed -i 's/@UpgradeBehaviourAptGetUpgrade@/'$(if [ $(grep UpgradeType=upgrade ~/.config/apt-notifierrc) ]; then echo -n true; else echo -n false; fi)'/' "$TMP"/DIALOG
+    sed -i 's/@UpgradeBehaviourAptGetDistUpgrade@/'$(if [ $(grep UpgradeType=dist-upgrade ~/.config/apt-notifierrc) ]; then echo -n true; else echo -n false; fi)'/' "$TMP"/DIALOG
+    sed -i 's/@LeftClickBehaviourSynaptic@/'$(if [ $(grep LeftClick=Synaptic ~/.config/apt-notifierrc) ]; then echo -n true; else echo -n false; fi)'/' "$TMP"/DIALOG
+    sed -i 's/@LeftClickBehaviourViewAndUpgrade@/'$(if [ $(grep LeftClick=ViewAndUpgrade ~/.config/apt-notifierrc) ]; then echo -n true; else echo -n false; fi)'/' "$TMP"/DIALOG
+    sed -i 's/@UpgradeAssumeYes@/'$(grep UpgradeAssumeYes ~/.config/apt-notifierrc | cut -f2 -d=)'/' "$TMP"/DIALOG
+    sed -i 's/@UpgradeAutoClose@/'$(grep UpgradeAutoClose ~/.config/apt-notifierrc | cut -f2 -d=)'/' "$TMP"/DIALOG
+
+
+    gtkdialog --file="$TMP"/DIALOG >> "$TMP"/output
+
+    grep -q EXIT=.*Apply.* "$TMP"/output
+
+    if [ "$?" -eq 0 ];
+      then
+        if [ $(grep UpgradeType_upgrade=.*true.*      "$TMP"/output) ]; then sed -i 's/UpgradeType=dist-upgrade/UpgradeType=upgrade/' ~/.config/apt-notifierrc; fi
+        if [ $(grep UpgradeType_dist-upgrade=.*true.* "$TMP"/output) ]; then sed -i 's/UpgradeType=upgrade/UpgradeType=dist-upgrade/' ~/.config/apt-notifierrc; fi
+        if [ $(grep LeftClickViewAndUpgrade=.*true.*  "$TMP"/output) ]; then sed -i 's/LeftClick=Synaptic/LeftClick=ViewAndUpgrade/'  ~/.config/apt-notifierrc; fi
+        if [ $(grep LeftClickSynaptic=.*true.*        "$TMP"/output) ]; then sed -i 's/LeftClick=ViewAndUpgrade/LeftClick=Synaptic/'  ~/.config/apt-notifierrc; fi
+        if [ $(grep UpgradeAssumeYes=.*false.*        "$TMP"/output) ]; then sed -i 's/UpgradeAssumeYes=true/UpgradeAssumeYes=false/' ~/.config/apt-notifierrc; fi
+        if [ $(grep UpgradeAssumeYes=.*true.*         "$TMP"/output) ]; then sed -i 's/UpgradeAssumeYes=false/UpgradeAssumeYes=true/' ~/.config/apt-notifierrc; fi
+        if [ $(grep UpgradeAutoClose=.*false.*        "$TMP"/output) ]; then sed -i 's/UpgradeAutoClose=true/UpgradeAutoClose=false/' ~/.config/apt-notifierrc; fi
+        if [ $(grep UpgradeAutoClose=.*true.*         "$TMP"/output) ]; then sed -i 's/UpgradeAutoClose=false/UpgradeAutoClose=true/' ~/.config/apt-notifierrc; fi
+      else
+        :
+    fi
+
+    rm -rf "$TMP"
+
+    '''
+    script_file = tempfile.NamedTemporaryFile('wt')
+    script_file.write(script)
+    script_file.flush()
+    run = subprocess.Popen(['sh %s' % script_file.name],shell=True).wait()
+    check_updates()
+
+# Define the command to run when left clicking on the Tray Icon
+def left_click():
+    if text.startswith( "0" ):
         start_synaptic()
+    else:
+        """Test ~/.config/apt-notifierrc for LeftClickViewAndUpgrade"""
+        command_string = "cat " + rc_file_name + " | grep -q LeftClick=ViewAndUpgrade"
+        exit_state = subprocess.call([command_string], shell=True, stdout=subprocess.PIPE)
+        if exit_state == 0:
+            showupdates()
+        else:
+            start_synaptic()
+
+# Define the action when left clicking on Tray Icon
+def left_click_activated(reason):
+    if reason == QtGui.QSystemTrayIcon.Trigger:
+        left_click()
 
 def read_icon_config():
     """Reads ~/.config/apt-notifierrc, returns 'show' if file doesn't exist or does not contain DontShowIcon"""
@@ -280,15 +470,21 @@ def set_noicon():
     AptIcon.hide()
     icon_config = "donot show"
 
-def add_upgrade_action():
+def add_rightclick_upgrade_action():
     ActionsMenu.clear()
-    show_updates_action = ActionsMenu.addAction("View and Upgrade")
-    AptNotify.connect(show_updates_action, QtCore.SIGNAL("triggered()"), showupdates)
-    #upgrade_action = ActionsMenu.addAction("Upgrade all packages")
-    #AptNotify.connect(upgrade_action, QtCore.SIGNAL("triggered()"), upgrade)
+    """Test ~/.config/apt-notifierrc for LeftClickViewAndUpgrade"""
+    command_string = "cat " + rc_file_name + " | grep -q LeftClick=ViewAndUpgrade"
+    exit_state = subprocess.call([command_string], shell=True, stdout=subprocess.PIPE)
+    if exit_state == 0:
+        process_updates_action = ActionsMenu.addAction("Upgrade using Synaptic")
+        AptNotify.connect(process_updates_action, QtCore.SIGNAL("triggered()"), start_synaptic)
+    else:
+        process_updates_action = ActionsMenu.addAction("View and Upgrade")
+        AptNotify.connect(process_updates_action, QtCore.SIGNAL("triggered()"), showupdates)
     add_apt_notifier_help_action()
     add_synaptic_help_action()
     add_quit_action()
+    add_aptnotifier_prefs_action()
 
 def add_hide_action():
     ActionsMenu.clear()
@@ -298,6 +494,7 @@ def add_hide_action():
     add_apt_notifier_help_action()
     add_synaptic_help_action()
     add_quit_action()
+    add_aptnotifier_prefs_action()
 
 def add_quit_action():
     ActionsMenu.addSeparator()
@@ -319,6 +516,12 @@ def add_synaptic_help_action():
     
 def open_synaptic_help():
     subprocess.Popen(['xdg-open http://www.mepiscommunity.org/doc_mx/synaptic.html'],shell=True)
+
+def add_aptnotifier_prefs_action():
+    ActionsMenu.addSeparator()
+    aptnotifier_prefs_action =  ActionsMenu.addAction("Apt Notifier Preferences")
+    AptNotify.connect(aptnotifier_prefs_action,QtCore.SIGNAL("triggered()"), aptnotifier_prefs)
+
 # General application code	
 def main():
     # Define Core objects, Tray icon and QTimer 
@@ -326,9 +529,10 @@ def main():
     global AptIcon
     global QuitIcon
     global icon_config
-    global upgrade_action
     global quit_action    
     global Timer
+    global initialize_aptnotifier_prefs
+    initialize_aptnotifier_prefs()
     AptNotify = QtGui.QApplication(sys.argv)
     AptIcon = QtGui.QSystemTrayIcon()
     Timer = QtCore.QTimer()
@@ -344,7 +548,7 @@ def main():
     # Create the right-click menu and add the Tooltip text
     global ActionsMenu
     ActionsMenu = QtGui.QMenu()
-    AptIcon.connect( AptIcon, QtCore.SIGNAL( "activated(QSystemTrayIcon::ActivationReason)" ), start_synaptic_activated)
+    AptIcon.connect( AptIcon, QtCore.SIGNAL( "activated(QSystemTrayIcon::ActivationReason)" ), left_click_activated)
     AptNotify.connect(Timer, QtCore.SIGNAL("timeout()"), check_updates)
     # Integrate it together,apply checking of updated packages and set timer to every 5 minutes (1 second = 1000)
     check_updates()
